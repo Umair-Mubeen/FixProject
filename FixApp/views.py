561bdd959b4e74004320904d5da48d83c4572a57
Umair-Mubeen <106667150+Umair-Mubeen@users.noreply.github.com>
2024-05-16 14:28:43 -0400

    Update views.py

--- FixApp/views.py
+++ FixApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
-import os, sys
+from django.shortcuts import render, HttpResponse, redirect
+import datetime
+import pandas as pd
+import numpy as np
 
 
 
@@ -21,6 +23,98 @@ def Logs(request):
     except IOError as e:
         print("Error Exception:", e)
         return HttpResponse("Internal Server Error", status=500)
+
+
+def OrderEvents(request):
+    try:
+        if request.method == 'POST':
+            CAT_IM_ID = request.POST['CAT_IM_ID']
+            FD_ID = request.POST['FD_ID']
+            Trading_Session = request.POST['Trading_Session']
+            file_Name = request.POST['file']
+            readCSV(file_Name, CAT_IM_ID, FD_ID, Trading_Session)
+            try:
+                return HttpResponse("CSV File has been created successfully")
+            except Exception as e:
+                return HttpResponse("Exception Error", str(e))
+        else:
+            return render(request, 'OrderEvents.html', )
+    except Exception as e:
+        print("Error Exception:", e)
+        return HttpResponse("Internal Server Error", status=500)
+
+
+def readCSV(fileName,CAT_IM_ID, FD_ID, Trading_Session):
+    filePath = "E:\\CAT Task\\" + fileName
+    df = pd.read_csv(filePath)
+    print(len(df))
+    print("CAT_IM_ID", CAT_IM_ID)
+    columns = ['Order Event', 'Fix_Col_0', 'FirmROID', 'MsgType', 'CAT_IM_ID',
+        'Date','Order ID','Symbol','TimeStamp','Fix_Col_1','Fix_Col_2','Fix_Col_3','Fix_Col_4','Fix_Col_5','Fix_Col_6',
+        'Fix_Col_7','Fix_Col_8','SideType','Price','Quantity','Fix_Col-9','OrderType','TIF','Trading Session','Fix_Col_10',
+        'Fix_Col_11','FDID','Acc Type','Fix_Col_12','Fix_Col_13','Fix_Col_14','Fix_Col_15','Fix_Col_16', 'Fix_Col_17',
+        'Fix_Col_18','Fix_Col_19','Fix_Col_20','Fix_Col_21','Fix_Col_22','Fix_Col_23','Fix_Col_24','Fix_Col_25','Fix_Col_26','Fix_Col_27','Fix_Col_28','Fix_Col_29'
+    ]
+    new_df = pd.DataFrame(columns=columns)
+    new_df['Fix_Col_0'] = ''
+    new_df['FirmROID'] = (pd.to_datetime(df['Event Timestamp']).dt.strftime('%Y%m%d').astype(str)+ "_" +CAT_IM_ID  + "-"
+                             + (df.index + 1).astype(str))
+    #new_df['Firm Row ID'] = pd.to_datetime(df['Event Timestamp']).dt.strftime('%Y%m%d').astype(str) + "_MVC_"
+    #new_df['Firm Row ID'] = pd.to_datetime(df['Event Timestamp']).dt.date  # update date
+    new_df['Order Event'] = 'NEW'
+
+    if df['Event Type'].eq('MEOA').all():  # check if Event Type has value MEOA replace with MENO
+        df.loc[df['Event Type'] == 'MEOA', 'Event Type'] = 'MENO'
+    new_df['MsgType'] = df['Event Type']
+    new_df['CAT_IM_ID'] = f'{CAT_IM_ID}'
+    new_df['Date'] = pd.to_datetime(df['Event Timestamp']).dt.strftime('%Y%m%d').astype(str) + "T000000.00000000"
+
+    #new_df['Date'] = pd.to_datetime(df['Event Timestamp']).dt.date
+    counter = 1
+    for index, row in df.iterrows():
+        new_df.loc[index, 'Order ID'] = "CAT-" + CAT_IM_ID + '-' + "OrderID-" +  f'{counter}'
+        counter += 1
+    new_df['TimeStamp'] = df['Event Timestamp']
+    new_df['Fix_Col_1'] = 'False'
+    new_df['Fix_Col_2'] = 'False'
+    new_df['Fix_Col_3'] = ''
+    new_df['Fix_Col_4'] = ''
+    new_df['Fix_Col_5'] = ''
+    new_df['Fix_Col_6'] = 'T'
+    new_df['Fix_Col_7'] = 'False'
+    new_df['Fix_Col_8'] = ''
+    new_df['SideType'] = df['Side']
+    if df['Price'].eq('').all():  # check if Price has no value then replace with empty
+        df.loc[df['Price'] == '', 'Price'] = ''
+    new_df['Symbol'] = df['Symbol']
+    new_df['Price'] = df['Price']
+    new_df['Quantity'] = df['Quantity']
+    new_df['OrderType'] = np.where(df['Price'].notnull(), 'LMT', 'MKT')
+    new_df['TIF'] = "GTX=" + pd.to_datetime(df['Event Timestamp']).dt.strftime('%Y%m%d').astype(str)
+    new_df['Trading Session'] = Trading_Session
+    new_df['Fix_Col_10'] = ''
+    new_df['Fix_Col_11'] = 'False'
+    new_df['FDID'] = FD_ID
+    new_df['Acc Type'] = 'A'
+    new_df['Fix_Col_12'] = 'False'
+    new_df['Fix_Col_13'] = ''
+    new_df['Fix_Col_14'] = ''
+    new_df['Fix_Col_15'] = 'False'
+    new_df['Fix_Col_16'] = 'N'
+    new_df['Fix_Col_17'] = ''
+    new_df['Fix_Col_18'] = ''
+    new_df['Fix_Col_19'] = ''
+    new_df['Fix_Col_20'] = ''
+    new_df['Fix_Col_21'] = ''
+    new_df['Fix_Col_22'] = ''
+    new_df['Fix_Col_23'] = ''
+    new_df['Fix_Col_24'] = ''
+    new_df['Fix_Col_25'] = ''
+    new_df['Fix_Col_26'] = ''
+    new_df['Fix_Col_27'] = ''
+    new_df['Fix_Col_28'] = ''
+    new_df['Fix_Col_29'] = ''
+    csv = new_df.to_csv('testing.csv', index=False)
 
 
 def find_ord_trails(file):
